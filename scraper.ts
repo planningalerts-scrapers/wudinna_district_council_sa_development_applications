79619bd4567d37f52fbc77df8405b75c6186b829
MichaelBone <Michael.Bone@senet.com.au>
2019-05-12 13:15:50 +0930

    Always save record as planningalerts now does versioning

--- scraper.ts
+++ scraper.ts
@@ -47,7 +47,7 @@ async function initializeDatabase() {
 
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
-        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?)");
+        let sqlStatement = database.prepare("insert or replace into [data] values (?, ?, ?, ?, ?, ?, ?)");
         sqlStatement.run([
             developmentApplication.applicationNumber,
             developmentApplication.address,
